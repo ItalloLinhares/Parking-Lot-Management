8786cc8aebe25eaca14d5823dc9a5e2f224055d8
ItalloLinhares <45083049+ItalloLinhares@users.noreply.github.com>
2021-12-23 19:20:16 -0300

    Initial commit

---
+++ historic/src/test/java/com/parking/historic/HistoricApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parking.historic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoricApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
